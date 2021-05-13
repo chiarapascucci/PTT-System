846dfea962143485d0f76ae37e6f6a9717841800
Chiara Pascucci <ch.pascucci@gmail.com>
2021-05-13 15:58:28 +0100

    change constructor as per request - committing to constructor change branch

--- PTTeacher.java
+++ PTTeacher.java
@@ -15,12 +15,13 @@ public class PTTeacher {
 	private String[] skills;
 	private String[] training;
 	private ArrayList <TeachRequest> assign;
-	private static ListOfPTT list; //-- invariant may be that if a teach exists then it must belong to a list
+	private static ListOfPTT list; //-- invariant may be that if a teacher exists then it must belong to a list
 	
 	//constructor
-	public PTTeacher (String f, String l) {
+	public PTTeacher (String f, String l, ListOfPTT j) {
 		this.fName = f;
 		this.lName = l;
+		this.list = j;
 		//this.year = y;
 		this.tID = nextTID;
 		nextTID ++;
@@ -30,23 +31,19 @@ public class PTTeacher {
 		this.skills = new String[10];
 		this.training = new String[10];
 		
-		listSetUp();
+		//listSetUp();
 		
 	}
 	
 	//method to manage teacher - list interaction
 	//if instance is first teacher being created then list is created with it, if not the teacher is just added to the list
 	//one invariant may be that if there is a teacher, then a list must exist to contain the teacher
-	private void listSetUp() {
-		if (this.tID == 0) {
-			list = new ListOfPTT();
-			list.addTeacher(this);
-		}
-		
-		else {
-			list.addTeacher(this);
-		}
-	}
+	/*
+	 * private void listSetUp() { if (this.tID == 0) { list = new ListOfPTT();
+	 * list.addTeacher(this); }
+	 * 
+	 * else { list.addTeacher(this); } }
+	 */
 
 	
 	
