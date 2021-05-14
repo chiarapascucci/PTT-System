adb2dc6cab2552369b4eded04b56e8b08f2cca6e
Vic J <55115156+Larkyth@users.noreply.github.com>
2021-05-14 13:41:54 +0100

    Update ListOfRequests.java
    ListOfRequest base code for now

--- ListOfRequests.java
+++ ListOfRequests.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
 
 public class ListOfRequests {
+	private ArrayList <TeachRequest> reqList;
 
+	public ListOfRequests() {
+		reqList = new ArrayList <TeachRequest> ();
+	}
+
+	public void printReqList() {
+		for(TeachRequest t: reqList) System.out.println(t);
+	}
 }
